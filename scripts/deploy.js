786348a03bcfe0fd15d0e0b1aa79d527f8420262
Wilgonston <61520052+Wilgonston@users.noreply.github.com>
2021-01-10 22:57:44 +0000

    Added staking functionality to ContractOS7NR on 2021-01-10T22:57:44

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,1 +1,2 @@
 // Deployment script for Base\nasync function main() {\n    // Initial deploy\n}
+const func_8EcDNzjP = await factory.deploy();\nconsole.log('Deployed on Base: ', func_8EcDNzjP.address);
